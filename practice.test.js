5a17b8c72b2d573494c553ec67e07c962926378e
Cassandra <semisentientfungus@gmail.com>
2024-08-23 15:09:04 +0200

    Create first suite of tests

--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,17 @@
 import {capitalise} from './practice';
 
-test('Capitalise single characters', () =>{
+test('Capitalise single characters, expect \'a\' to return \'A\'', () =>{
     expect(capitalise('a')).toBe('A');
+})
+
+test('Capitalise string, expect \'a string\' to return \'A string\'', () => {
+    expect(capitalise('a string')).toBe('A string');
+})
+
+test('Capitalise string with multiple letters in the first word, expect \'an idea\' to return \'An idea\'', () =>{
+    expect(capitalise('an idea')).toBe('An idea');
+})
+
+test('Returns capital when first letter is already capitalised', () =>{
+    expect(capitalise('Already done')).toBe('Already done');
 })
