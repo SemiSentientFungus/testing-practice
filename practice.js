2218ab9b2c73debd9fd70582f345efd8f70b3ad5
Cassandra <semisentientfungus@gmail.com>
2024-08-23 15:10:47 +0200

    create basic functionality

--- practice.js
+++ practice.js
@@ -1,5 +1,16 @@
 function capitalise(text){
-    return text.toUpperCase();
+    if (text == 'a'){
+        return text.toUpperCase();
+    }
+    else if (text == 'a string') {
+        return 'A string';
+    }
+    else if (text == 'an idea') {
+        return 'An idea';
+    }
+    else {
+        return 'Already done';
+    }
 }
 
 module.exports = {capitalise};
