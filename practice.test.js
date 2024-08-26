37fbbbcfdaf489d83a1659f672eebe4804888c4d
Cassidy <semisentientfungus@gmail.com>
2024-08-26 15:02:40 +0200

    add second test to calculator.add

--- practice.test.js
+++ practice.test.js
@@ -39,3 +39,7 @@ test('Does calculator exist?', () => {
 test('2 + 2 equals 4', () => {
     expect(calculator.add(2, 2)).toBe(4);
 })
+
+test('2 + -2 equals 0', () => {
+    expect(calculator.add(2, -2)).toBe(0);
+})
