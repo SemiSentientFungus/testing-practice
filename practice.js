63070a2c7d697b2e0bb9aca8861f87925bf30e0e
Cassidy <semisentientfungus@gmail.com>
2024-08-26 15:26:01 +0200

    create functionality for divide and multiply functions

--- practice.js
+++ practice.js
@@ -9,6 +9,15 @@ function reverseString(text){
 const calculator = {
     add(a, b) {
         return a + b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+    divide(a, b) {
+        return a / b;
+    },
+    multiply(a, b) {
+        return a * b;
     }
 }
 
